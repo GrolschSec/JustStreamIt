3f71734c583c9eebcf5c3ea4e48bd8cbfa6034e2
Romain <romain@rtfm.home>
2022-08-30 20:13:45 +0200

    fetchCategory working

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,15 @@
 url = "http://127.0.0.1:8000/api/v1/titles";
 
-function fetchModal(id, element){	
-	let img_div = element.getElementsByClassName("movie__img")[0];
-	let content = element.getElementsByClassName("movie-info")[0];
+function fetchModal(id, element){
+
+	let img_div = element.getElementsByClassName("modal__img")[0];
+	let content = element.getElementsByClassName("modal__data")[0];
 	let info = {
-		"date_published": 'Release Date: ',
-		"duration": 'Duration: ',
-		"rated": 'Rated: ',
 		"imdb_score": 'IMDB Score: ',
 		"avg_vote": 'Box office: ',
+		"duration": 'Duration (min): ',
+		"date_published": 'Release Date: ',
+		"rated": 'Rated: ',
 		"description": 'Description: '
 	}
 	let single_list = {
@@ -20,16 +21,22 @@ function fetchModal(id, element){
 		"genres": "Genres: ",
 	}
 
+
 	fetch(url + '/' + id)
 	.then(response => response.json())
 	.then(data => {
-		img_div.appendChild(document.createElement('img')).setAttribute('src', data['image_url']);
-		content.appendChild(document.createElement('h1')).textContent = data['title'];
+		img_div.appendChild(document.createElement('img'))
+		.setAttribute('src', data['image_url']);
+		content.appendChild(document.createElement('h1'))
+		.textContent = data['title'];
 		for (const [key, value] of Object.entries(info)) {
-			content.appendChild(document.createElement('p')).textContent = `${value}${data[key]}`
+			content.appendChild(document.createElement('p'))
+			.setAttribute('class', `modal__${key}`);
+			content.getElementsByClassName(`modal__${key}`)[0]
+			.textContent = `${value}${data[key]}`;
 		}
 		for (const [key, value] of Object.entries(single_list)) {
-			content.appendChild(document.createElement('p')).textContent = `${value}${data[key][0]}`
+			content.appendChild(document.createElement('p')).textContent = `${value}${data[key][0]}`;
 		}
 		let i = 0;
 		let list_el = ''
@@ -48,9 +55,10 @@ function fetchModal(id, element){
 function openModal(id, element){
 
 	let modal = element.getElementsByClassName('modal')[0];
-	modal.innerHTML = "<div class='modal__content'><div class='movie-info'></div><div class='modal__img-cls'><div class='movie__img'></div><span class='modal--close'>&times</span></div><div/>";
+	modal.innerHTML = "<div class='modal__content'><div class='modal__data'></div><div class='modal__img-cls'><div class='modal__img'></div><span class='modal--close'>&times</span></div><div/>";
 	fetchModal(id, modal);
 	let span = element.getElementsByClassName('modal--close')[0];
+
 
 	modal.classList.add("modal--show");
 	window.onclick = function(event) {
@@ -62,7 +70,7 @@ function openModal(id, element){
 	span.onclick = function(){
 		modal.classList.remove("modal--show");
 		modal.innerHTML = ""
-	}
+	};
 }
 
 function fetchBestRatedMovie(){
@@ -74,6 +82,7 @@ function fetchBestRatedMovie(){
 	let description = bestMovie.getElementsByTagName('p')[0];
 	let btn = bestMovie.getElementsByClassName('modal__button')[0];
 	
+
 	fetch(url + "?sort_by=-imdb_score")
 		.then(response => response.json())
 		.then(data => {
@@ -90,5 +99,21 @@ function fetchBestRatedMovie(){
 	});
 }
 
+async function fetchCategory(categoryName = '', i = 0){
 
-fetchBestRatedMovie('');
+	const response = await fetch(url + '?sort_by=-imdb_score' + '&genre=' + categoryName);
+	const data = await response.json()
+	let categoryData = Array(...data.results);
+
+
+	if (i > 0)
+        categoryData.splice(0, i);
+	if (categoryData.length < 7){
+		let data2 = await (await fetch(data.next)).json();
+        categoryData.push(...Array(...data2.results).slice(0, 7 - categoryData.length));
+	}
+	return categoryData
+}
+
+console.log(fetchCategory('', 1), fetchCategory('action'), fetchCategory('biography'), fetchCategory('historic'));
+fetchBestRatedMovie();
